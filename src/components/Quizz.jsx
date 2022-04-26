f20aa037917c249787125fcb7f03e953e3eed1a8
Jeton <90478926+JetonShabani@users.noreply.github.com>
2022-04-26 11:39:53 +0200

    initialized app

---
+++ src/components/Quizz.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function Trivia() {
+  return (
+    <div>
+        <div className="trivia">
+            <div className="question">What`s the best job?</div>
+        </div>
+        <div className="answers">
+            <div className="answer">web dev</div>
+            <div className="answer">hr</div>
+            <div className="answer">you</div>
+            <div className="answer">me</div>
+        </div>
+    </div>
+  )
+}

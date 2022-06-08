3ca3d74eb506a65cc4ed1a9127f6b5c5d4f51fab
Jeton <90478926+JetonShabani@users.noreply.github.com>
2022-06-08 09:37:10 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,20 +7,35 @@ function App() {
 //all in the right 
   const moneyPyramid = [
     { id:1, amount:"€ 100,00" },
+
     { id:2, amount:"€ 200,00" },
+
     { id:3, amount:"€ 300,00" },
+
     { id:4, amount:"€ 500,00" },
+
     { id:5, amount:"€ 1.000,00" },
+
     { id:6, amount:"€ 2.000,00" },
+
     { id:7, amount:"€ 4.000,00" },
+
     { id:8, amount:"€ 8.000,00" },
+
     { id:9, amount:"€ 16.000,00" },
+
     { id:10, amount:"€ 32.000,00" },
+
     { id:11, amount:"€ 64.000,00" },
+
     { id:12, amount:"€ 125.000,00" },
+
     { id:13, amount:"€ 250.000,00" },
+
     { id:14, amount:"€ 500.000,00" },
+
     { id:15, amount:"€ 1.000.000,00" },
+    
   ].reverse();
 
   return (
